0346148ceca0a424c86043a833b4b898e798c7d0
chaehaeun <chae_3210@naver.com>
2023-07-04 02:24:37 +0900

    [add] 8.15 스타일링 하기 - 투두 아이템

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import styles from "./AddTodo.module.css";
 
 const AddTodo = ({ onAdd }) => {
   const [text, setText] = useState("");
@@ -16,14 +17,17 @@ const AddTodo = ({ onAdd }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <input
+        className={styles.input}
         type="text"
         placeholder="Add Todo"
         value={text}
         onChange={handleChange}
       />
-      <button type="submit">Add</button>
+      <button type="submit" className={styles.button}>
+        Add
+      </button>
     </form>
   );
 };
