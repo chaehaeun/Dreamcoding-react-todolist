9a67b546e2c7eef8bea54b0a8b5637eb31768706
chaehaeun <chae_3210@naver.com>
2023-07-04 15:30:27 +0900

    [add] 8.18 다크모드 셋팅 기억하기

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { useDarkMode } from "../../context/DarkModeContext";
+import { HiSun, HiMoon } from "react-icons/hi";
 
 const Header = ({ filters, filter, onFilterChange }) => {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <header className={styles.header}>
+      <button onClick={toggleDarkMode}>
+        {!darkMode ? <HiMoon /> : <HiSun />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
